8c9d33b23c713588c3d8a8b01369b78ad52485df
pavel <Bulbashful@gmail.com>
2018-12-18 20:40:21 +0300

    Code refactor. Added output to tablewidget. Much changes

--- client.py
+++ client.py
@@ -11,79 +11,33 @@ import sys
 import os
 import pathlib
 import time
+import re
 
 
 class ExampleApp(QtWidgets.QMainWindow, design.Ui_Form):
+
     def __init__(self):
         # Это здесь нужно для доступа к переменным, методам
         # и т.д. в файле design.py
         super().__init__()
         self.setupUi(self)  # Это нужно для инициализации нашего дизайна
-        self.browse_folder()
-        # текущая дериктория
         # занесение файлов в list widget и прикпреплние иконки в зависимости от типа файла
-        self.local_files()
-        self.server_files()
-        self.listWidget_2.dragEnabled()
+        # передать файлы текущеё рабочей директории
+        self.tableWidget.local_files(os.listdir(os.getcwd()))
+        self.hostWidget.server_files()
+        self.tableWidget.cellClicked.connect(self.cellClick)
+        #self.listWidget_2.dragEnabled()
+
+    def cellClick(self, p_int, p_int_1):
+        if p_int == 0:
+            above_path = os.chdir(os.path.dirname(os.getcwd()))
+            self.tableWidget.local_files(os.listdir(above_path))
 
 
-    def local_files(self):
-        current_path = os.getcwd()
-        path_items = os.listdir(current_path)
-
-        self.tableWidget.setRowCount(len(path_items))
-        self.tableWidget.setColumnCount(3)
-        self.tableWidget.setHorizontalHeaderLabels(['File', 'Last modified data'])
-        self.tableWidget.setDragEnabled(True)
-        self.listWidget_2.setAcceptDrops(True)
-
-        for num, file in enumerate(path_items):
-            modified_time = os.path.getmtime(file)
-            custom_modified_time = time.strftime('%b %d %Y %H:%M:%S', time.localtime(modified_time))
-            a = file + " " * (30-len(file)) + custom_modified_time
-            print(a)
-            item = QListWidgetItem(a)
-            icon = 'icons\dir_icon.png' if os.path.isdir(file) else r'icons\file_icon.png'
-            #item.setTextAlignment(Qt.AlignBaseline)
-            item.setTextAlignment(Qt.RightToLeft)
-            item.setIcon(QIcon(icon))
-            item.setFlags(QtCore.Qt.ItemIsDragEnabled)
-            #.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
-            self.tableWidget.setEditTriggers(QAbstractItemView.NoEditTriggers)
-            self.tableWidget.setItem(num, 0, QTableWidgetItem(file))
-            self.tableWidget.setItem(num, 1, QTableWidgetItem(custom_modified_time))
-            self.listWidget_2.addItem(item)
-
-        #self.tableWidget.setSelectionMode(QAbstractItemView.ExtendedSelection)
-        #self.tableWidget.setFocusPolicy(QtCore.Qt.NoFocus)
-        #self.tableWidget.setFrameStyle(QFrame.NoFrame)
-        self.tableWidget.setShowGrid(False)
-        #self.tableWidget.show()
 
 
-    def server_files(self):
-        ftp = FTP('92.242.39.60')
-        ftp.login()
-        files_list = []
-        # callback функция для занесения файлв в список вместо стандартного stdout callback
-        custom_callback = lambda n: files_list.append(n)
-        # получить список файлов с сервера
-        ftp.retrlines('LIST', custom_callback)
-        for file in files_list:
-            item = QListWidgetItem(file)
-            icon = 'icons\dir_icon.png' if file[0] == 'd' else r'icons\file_icon.png'
-            item.setIcon(QIcon(icon))
-            self.listWidget_3.addItem(item)
 
-    def browse_folder(self):
-        self.listWidget.clear()  # На случай, если в списке уже есть элементы
-        #directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
-        # открыть диалог выбора директории и установить значение переменной
-        # равной пути к выбранной директории
 
-        # if directory:  # не продолжать выполнение, если пользователь не выбрал директорию
-        #     for file_name in os.listdir(directory):  # для каждого файла в директории
-        #         self.listWidget.addItem(file_name)  # добавить файл в listWidget
 
 def main():
     app = QtWidgets.QApplication(sys.argv)  # Новый экземпляр QApplication
--- design.py
+++ design.py
@@ -8,22 +8,123 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import Qt
+import os
+from PyQt5.QtWidgets import QWidget, QPushButton, QApplication, QListWidgetItem, QHeaderView, QMainWindow, \
+    QGridLayout, QTableWidget, QTableWidgetItem, QFrame, QAbstractItemView
+import time
+from ftplib import FTP
+import re
+from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QTableWidget, QTableWidgetItem
 
 
-class Cas(QTableWidget):
+class LocalTable(QTableWidget):
 
     def __init__(self, *__args):
         QTableWidget.__init__(self, *__args)
+        global current_path
+        current_path = os.getcwd()
+        self.setColumnCount(3)
+        self.setHorizontalHeaderLabels(['File', 'Last modified date'])
+        self.setDragEnabled(True)
 
     def mouseReleaseEvent(self, *args, **kwargs):
-        print(self.currentRow())
-        self.selectRow(self.currentRow())
-        super(Cas, self).mouseReleaseEvent(*args, **kwargs)
+        #print(self.currentRow())
+        # self.selectRow(self.currentRow())
+        super(LocalTable, self).mouseReleaseEvent(*args, **kwargs)
+        #super(LocalTable, self).mouseReleaseEvent(*args, **kwargs)
 
     def dragMoveEvent(self, e):
         print('=df')
         e.accept()
+
+    # def cellDoubleClicked(self, p_int, p_int_1):
+    #     print('dfdf')
+
+
+
+    def mouseDoubleClickEvent(self, *args, **kwargs):
+        #os.chdir(os.path.dirname(os.getcwd()))
+        #print('new ', os.getcwd())
+        # if self.mouseReleaseEvent(*args, **kwargs):
+        #     self.local_files(os.getcwd())
+        super(LocalTable, self).mouseDoubleClickEvent(*args, **kwargs)
+
+    def local_files(self, path_items):
+        path_items = os.listdir(path_items)
+        print(path_items)
+        self.setRowCount(len(path_items)+1)
+
+        for num, file in enumerate(path_items, start=1):
+            modified_time = os.path.getmtime(file)
+            custom_modified_time = time.strftime('%b %d %Y', time.localtime(modified_time))
+            item = QTableWidgetItem(file)
+            icon = f'{current_path}\icons\{"dir_icon.png" if os.path.isdir(file) else "file_icon.png"}'
+            item.setIcon(QIcon(icon))
+            #item.setTextAlignment(Qt.AlignBaseline)
+            #item.setTextAlignment(Qt.RightToLeft)
+
+            #item.setFlags(QtCore.Qt.ItemIsDragEnabled)
+
+            #.setFlags(QtCore.Qt.ItemIsSelectable | QtCore.Qt.ItemIsEnabled)
+            self.setEditTriggers(QAbstractItemView.NoEditTriggers)
+            self.setSelectionBehavior(QTableWidget.SelectRows)
+            self.setItem(num, 0, QTableWidgetItem(item))
+            self.setItem(num, 1, QTableWidgetItem(custom_modified_time))
+
+        #self.tableWidget.setSelectionMode(QAbstractItemView.ExtendedSelection)
+        #self.tableWidget.setFocusPolicy(QtCore.Qt.NoFocus)
+        #self.tableWidget.setFrameStyle(QFrame.NoFrame)
+        self.setShowGrid(False)
+
+
+class HostTable(QTableWidget):
+
+    def __init__(self, *__args):
+        QTableWidget.__init__(self, *__args)
+        self.setColumnCount(3)
+        self.setHorizontalHeaderLabels(['File', 'Last modified date'])
+        self.setDragEnabled(True)
+        self.setAcceptDrops(True)
+
+    def mouseReleaseEvent(self, *args, **kwargs):
+        print(self.currentRow())
+        self.selectRow(self.currentRow())
+        super(HostTable, self).mouseReleaseEvent(*args, **kwargs)
+
+    def dragMoveEvent(self, e):
+        print('=df')
+        e.accept()
+
+    def mouseDoubleClickEvent(self, *args, **kwargs):
+        print(os.getcwd())
+        print(*args, **kwargs)
+        super(HostTable, self).mouseDoubleClickEvent(*args, **kwargs)
+
+    def server_files(self):
+        ftp = FTP('92.242.39.60')
+        ftp.login()
+        files_list = []
+        # callback функция для занесения файлв в список вместо стандартного stdout callback
+        custom_callback = lambda n: files_list.append(n)
+        # получить список файлов с сервера
+        ftp.retrlines('LIST', custom_callback)
+
+        self.setRowCount(len(files_list) + 1)
+
+        for num, file in enumerate(files_list):
+            data = re.findall(r'\d+ \w+ \w+  \w+ \w+.*', file)[0].split(' ')
+            data.pop(3)
+
+            file_name = QTableWidgetItem(data[4])
+            modified_time = QTableWidgetItem(data[2] + ' ' + data[1] + ' ' + data[2])
+            icon = 'icons\dir_icon.png' if file[0] == 'd' else r'icons\file_icon.png'
+            file_name.setIcon(QIcon(icon))
+            #print(data)
+            self.setItem(num, 0, QTableWidgetItem(file_name))
+            self.setItem(num, 1, QTableWidgetItem(modified_time))
+        self.setShowGrid(False)
+
 
 class Ui_Form(object):
     def setupUi(self, Form):
@@ -36,14 +137,11 @@ class Ui_Form(object):
         self.listWidget = QtWidgets.QListWidget(Form)
         self.listWidget.setGeometry(QtCore.QRect(20, 340, 891, 231))
         self.listWidget.setObjectName("listWidget")
-        self.listWidget_3 = QtWidgets.QListWidget(Form)
-        self.listWidget_3.setGeometry(QtCore.QRect(480, 40, 431, 241))
-        self.listWidget_3.setObjectName("listWidget_3")
-        self.listWidget_2 = QtWidgets.QListWidget(Form)
-        self.listWidget_2.setGeometry(QtCore.QRect(20, 40, 431, 241))
-        self.listWidget_2.setObjectName("listWidget_2")
-        self.tableWidget = Cas(Form)
-        self.tableWidget.setGeometry(QtCore.QRect(100, 280, 656, 192))
+        self.hostWidget = HostTable(Form)
+        self.hostWidget.setGeometry(QtCore.QRect(480, 40, 431, 241))
+        self.hostWidget.setObjectName("hostWidget")
+        self.tableWidget = LocalTable(Form)
+        self.tableWidget.setGeometry(QtCore.QRect(20, 40, 431, 241))
         self.tableWidget.setObjectName("tableWidget")
 
         self.retranslateUi(Form)
